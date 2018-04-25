cae4420d6cd407dac805055d3ba1620484856302
Unknown <s.gsaenger@gmx.de>
2018-04-25 14:40:30 +0200

    Project Setup
    Unreal Project

---
+++ MyProject/Intermediate/Build/Win64/UE4Editor/Inc/MyProject/VRCharacter.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef MYPROJECT_VRCharacter_generated_h
+#error "VRCharacter.generated.h already included, missing '#pragma once' in VRCharacter.h"
+#endif
+#define MYPROJECT_VRCharacter_generated_h
+
+#define MyProject_Source_MyProject_VRCharacter_h_12_RPC_WRAPPERS
+#define MyProject_Source_MyProject_VRCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define MyProject_Source_MyProject_VRCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAVRCharacter(); \
+	friend MYPROJECT_API class UClass* Z_Construct_UClass_AVRCharacter(); \
+public: \
+	DECLARE_CLASS(AVRCharacter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/MyProject"), NO_API) \
+	DECLARE_SERIALIZER(AVRCharacter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define MyProject_Source_MyProject_VRCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAVRCharacter(); \
+	friend MYPROJECT_API class UClass* Z_Construct_UClass_AVRCharacter(); \
+public: \
+	DECLARE_CLASS(AVRCharacter, ACharacter, COMPILED_IN_FLAGS(0), 0, TEXT("/Script/MyProject"), NO_API) \
+	DECLARE_SERIALIZER(AVRCharacter) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define MyProject_Source_MyProject_VRCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AVRCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AVRCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRCharacter(AVRCharacter&&); \
+	NO_API AVRCharacter(const AVRCharacter&); \
+public:
+
+
+#define MyProject_Source_MyProject_VRCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AVRCharacter(AVRCharacter&&); \
+	NO_API AVRCharacter(const AVRCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AVRCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AVRCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AVRCharacter)
+
+
+#define MyProject_Source_MyProject_VRCharacter_h_12_PRIVATE_PROPERTY_OFFSET
+#define MyProject_Source_MyProject_VRCharacter_h_9_PROLOG
+#define MyProject_Source_MyProject_VRCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyProject_Source_MyProject_VRCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	MyProject_Source_MyProject_VRCharacter_h_12_RPC_WRAPPERS \
+	MyProject_Source_MyProject_VRCharacter_h_12_INCLASS \
+	MyProject_Source_MyProject_VRCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define MyProject_Source_MyProject_VRCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	MyProject_Source_MyProject_VRCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	MyProject_Source_MyProject_VRCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	MyProject_Source_MyProject_VRCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	MyProject_Source_MyProject_VRCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID MyProject_Source_MyProject_VRCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
